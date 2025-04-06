49c1f76444b5b4dce80cb083f74a02b297891bda
maxdevfranklin <max.franklin.tech@gmail.com>
2025-04-06 04:25:02 -0400

    init project

--- server.js
+++ server.js
@@ -3,17 +3,19 @@ const cors = require('cors');
 const path = require('path');
 const app = express();
 
-app.use(cors()); // This will allow all origins
-// Serve the index.html file when the root URL is accessed
+// Enable CORS
+app.use(cors());
 
+// Serve static files from the 'public' directory
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', (req, res) => {
+// Serve index.html for all routes to support SPA routing
+app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 // Set the port to listen on
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
