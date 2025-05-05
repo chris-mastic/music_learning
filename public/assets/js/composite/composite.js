bc79b911fd3c750a3af14356c649ad40a926d64e
maxdevfranklin <max.franklin.tech@gmail.com>
2025-05-05 06:17:08 -0500

    add sync between button & sheet

--- public/assets/js/composite/composite.js
+++ public/assets/js/composite/composite.js
@@ -13,9 +13,9 @@ const noteColor = [
 let noteGroup = [];
 for (let i = 0; i < 32; i++) {
   noteGroup.push([]);
-  if (i == 0) noteGroup[0].push(0);
 }
-const notePair = ["c/4", "d/4", "e/4", "f/4", "g/4", "a/4", "b/4"];
+const notePair = ["c/4", "d/4", "e/4", "f/4", "g/4", "a/4", "b/4", "c/5"];
+const EasePair = ["qr", "q", "q", "q", "q", "q", "q", "q"];
 
 const gridContainer = document.getElementById("noteGroup");
 
@@ -28,14 +28,21 @@ for (let i = 1; i <= 10 * numColumns; i++) {
   indexColumn = i % numColumns;
   indexRow = Math.floor(i / numColumns);
   if (i <= 8 * numColumns) {
-    if (Math.floor((indexColumn - 1) / 4) % 2) {
+    if (Math.floor((indexColumn - 1) / 8) % 2) {
       button.classList.add("oddBtn");
     } else {
       button.classList.add("evenBtn");
     }
 
+    if ((indexColumn - 1) % 2 && indexColumn != 0) {
+      button.classList.add("mainDivider");
+    }
+
     button.addEventListener("mousedown", () => {
       button.classList.toggle("selected");
+      const index = button.getAttribute("data-id");
+      buttonRow = 7 - Math.floor(index / numColumns);
+      buttonColumn = (index - 1) % numColumns;
       if (button.classList.contains("selected")) {
         if (!button.hasAttribute("data-original-bg")) {
           button.setAttribute(
@@ -43,15 +50,24 @@ for (let i = 1; i <= 10 * numColumns; i++) {
             button.style.backgroundColor || ""
           );
         }
-        const index = button.getAttribute("data-id");
-        buttonRow = 7 - Math.floor(index / numColumns);
+        button.setAttribute("border", "none");
         button.style.backgroundColor = noteColor[buttonRow];
         // button.innerHTML = buttonNote[buttonRow];
 
         //play audio
+
+        //impact music sheet
+        noteGroup[buttonColumn].push(buttonRow);
+        // console.log(buttonColumn, buttonRow + 1);
+        drawVex();
       } else {
         const originalBg = button.getAttribute("data-original-bg");
         button.style.backgroundColor = originalBg || "";
+        for (let k = 0; k < noteGroup[buttonColumn].length; k++) {
+          if (noteGroup[buttonColumn][k] == buttonRow)
+            noteGroup[buttonColumn].splice(k, 1);
+        }
+        drawVex();
       }
     });
 
@@ -69,84 +85,116 @@ for (let i = 1; i <= 10 * numColumns; i++) {
     }
     if (i > 7 * numColumns) button.style.marginBottom = "30px";
   } else {
+    button.style.width = "50%";
+    button.style.height = "50%";
+    button.style.marginLeft = "25%";
+    button.style.marginTop = "25%";
     button.style.borderRadius = "50%";
-    button.style.width = "30%";
-    button.style.height = "30%";
-    button.style.marginLeft = "35%";
     button.style.backgroundColor = "#ccc";
     // gridContainer.style.gap = "10px";
+    button.addEventListener("click", () => {
+      const index = button.getAttribute("data-id");
+      buttonRow = Math.floor(index / numColumns);
+
+      console.log(buttonRow);
+      button.classList.toggle("selected");
+      if (button.classList.contains("selected")) {
+        button.style.width = "70%";
+        button.style.height = "65%";
+        button.style.marginLeft = "15%";
+        button.style.marginTop = "15%";
+        button.style.backgroundColor = "#16a8f0";
+        button.style.borderLeft = "0px";
+        button.style.borderTop = "0px";
+        if (buttonRow == 8) button.style.borderRadius = "20%";
+      } else {
+        button.style.width = "50%";
+        button.style.height = "50%";
+        button.style.marginLeft = "25%";
+        button.style.backgroundColor = "#ccc";
+        button.style.borderRadius = "50%";
+      }
+    });
     gridContainer.appendChild(button);
   }
 }
 
-// Initialize VexFlow
-const VF = Vex.Flow;
-const div = document.getElementById("notation");
-const renderer = new VF.Renderer(div, VF.Renderer.Backends.SVG);
+drawVex();
 
-renderer.resize(1300, 200);
-const context = renderer.getContext();
+function drawVex() {
+  // Initialize VexFlow
+  const div = document.getElementById("notation");
+  div.innerHTML = "";
 
-// Create a stave (staff)
-const stave = new VF.Stave(10, 20, 160);
-stave.addClef("treble").addTimeSignature("4/4");
-stave.setContext(context).draw();
+  const VF = Vex.Flow;
+  const renderer = new VF.Renderer(div, VF.Renderer.Backends.SVG);
 
-var notes = [
-  // A quarter-note C.
-  new VF.StaveNote({ clef: "treble", keys: ["a/4"], duration: "q" }),
-  new VF.StaveNote({ clef: "treble", keys: ["b/4"], duration: "qr" }),
-  new VF.StaveNote({ clef: "treble", keys: ["b/4"], duration: "qr" }),
-  new VF.StaveNote({ clef: "treble", keys: ["b/4"], duration: "qr" }),
-  // new VF.StaveNote({
-  //   clef: "treble",
-  //   keys: ["c/4", "e/4", "g/4"],
-  //   duration: "q",
-  // }),
-];
+  sheetLength = 1300;
 
-var voice = new VF.Voice({ num_beats: 4, beat_value: 4 });
-voice.addTickables(notes);
+  renderer.resize(sheetLength, 200);
+  const context = renderer.getContext();
 
-// Format and justify the notes to 400 pixels.
-var formatter = new VF.Formatter().joinVoices([voice]).format([voice], 85);
+  let stave = new VF.Stave(10, 20, sheetLength - 20);
+  stave.setContext(context).draw();
 
-// Render voice
-voice.draw(context, stave);
+  for (let i = 0; i < 32; i++) {
+    let stave1 = new VF.Stave(10, 20, 38);
+    if (i > 0) {
+      stave1 = new VF.Stave(38 * i + 74, 20, 38);
+    }
+    if (i < 31) {
+      stave1.setBegBarType(VF.Barline.type.NONE);
+      stave1.setEndBarType(VF.Barline.type.NONE);
+    }
+    stave1.setBegBarType(VF.Barline.type.NONE);
+    if (i == 0) {
+      stave1.addClef("treble").addTimeSignature("4/4");
+    }
+    stave1.setContext(context).draw();
 
-for (let i = 1; i < 8; i++) {
-  let stave1 = new VF.Stave(stave.width * i + stave.x, 20, 160);
-  stave1.setContext(context).draw();
+    var notes = [
+      // A quarter-note C.
+      // new VF.StaveNote({
+      //   clef: "treble",
+      //   keys: [notePair[noteGroup[i]]],
+      //   duration: EasePair[noteGroup[i]],
+      // }),
+      // new VF.StaveNote({
+      //   clef: "treble",
+      //   keys: ["c/4", "e/4", "g/4"],
+      //   duration: "q",
+      // }),
+    ];
 
-  var notes = [
-    // A quarter-note C.
-    new VF.StaveNote({ clef: "treble", keys: ["a/4"], duration: "q" }),
-    new VF.StaveNote({ clef: "treble", keys: ["b/4"], duration: "qr" }),
-    new VF.StaveNote({ clef: "treble", keys: ["b/4"], duration: "qr" }),
-    new VF.StaveNote({ clef: "treble", keys: ["b/4"], duration: "qr" }),
-    // new VF.StaveNote({
-    //   clef: "treble",
-    //   keys: ["c/4", "e/4", "g/4"],
-    //   duration: "q",
-    // }),
-  ];
+    if (noteGroup[i].length > 0) {
+      // if (noteGroup[i][j] == "0") {
+      tempKeys = [];
+      for (k = 0; k < noteGroup[i].length; k++) {
+        tempKeys.push(notePair[noteGroup[i][k]]);
+      }
+      notes.push(
+        new VF.StaveNote({
+          clef: "treble",
+          keys: tempKeys,
+          duration: "q",
+        })
+      );
+      // }
+    }
+    // console.log(noteGroup);
 
-  // for (let i = 0; i < 32; i++) {
-  //   if (noteGroup[i] != "") {
-  //     notes.push(
-  //       new VF.StaveNote({ clef: "treble", keys: ["cb/4"], duration: "qr" })
-  //     );
-  //   }
-  // }
-  // console.log(noteGroup);
+    // // Create a voice in 4/4 and add above notes
+    if (notes.length > 0) {
+      var voice = new VF.Voice({ num_beats: 1, beat_value: 4 });
+      voice.addTickables(notes);
 
-  // // Create a voice in 4/4 and add above notes
-  var voice = new VF.Voice({ num_beats: 4, beat_value: 4 });
-  voice.addTickables(notes);
+      // Format and justify the notes to 400 pixels.
+      var formatter = new VF.Formatter()
+        .joinVoices([voice])
+        .format([voice], 40);
 
-  // Format and justify the notes to 400 pixels.
-  var formatter = new VF.Formatter().joinVoices([voice]).format([voice], 140);
-
-  // Render voice
-  voice.draw(context, stave1);
+      // Render voice
+      voice.draw(context, stave1);
+    }
+  }
 }
